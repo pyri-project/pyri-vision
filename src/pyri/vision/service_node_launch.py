54079799730f6148ceff83de191c82ee46788200
John Wason <wason@wasontech.com>
2021-05-26 01:30:44 -0400

    Fix default service info

--- src/pyri/vision/service_node_launch.py
+++ src/pyri/vision/service_node_launch.py
@@ -7,9 +7,9 @@ launches = [
     ServiceNodeLaunch("vision_camera_calibration", "pyri.vision", "pyri.vision.camera_calibration_service",\
         default_devices=[("pyri_camera_calibration_service","vision_camera_calibration")]),
     ServiceNodeLaunch("vision_viewer_service", "pyri.vision", "pyri.vision.camera_viewer_service",\
-        default_devices=["pyri_camera_viewer_service","vision_camera_viewer"]),
+        default_devices=[("pyri_camera_viewer_service","vision_camera_viewer")]),
     ServiceNodeLaunch("vision_robot_calibration", "pyri.vision", "pyri.vision.robot_calibration_service",\
-        default_devices=[("pyri_robot_calibration_service","vision_robot_calibration")]),
+        default_devices=[("pyri_vision_robot_calibration_service","vision_robot_calibration")]),
     ServiceNodeLaunch("vision_template_matching", "pyri.vision", "pyri.vision.template_matching",\
         default_devices=[("pyri_template_matching_service","vision_template_matching")]),
 ]
