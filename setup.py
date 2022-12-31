28ff1093adb2a3ed1a5c6e4ea903b190df3920b9
John Wason <wason@wasontech.com>
2022-12-31 03:32:34 -0500

    Use pyproject.toml

--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
 from setuptools import setup, find_packages, find_namespace_packages
 
 setup(
-    name='pyri-vision',
-    version='0.1.0',
-    description='PyRI Teach Pendant Vision Plugin',
-    author='John Wason',
-    author_email='wason@wasontech.com',
-    url='http://pyri.tech',
     package_dir={'': 'src'},
     packages=find_namespace_packages(where='src'),
     include_package_data=True,
@@ -17,28 +11,5 @@ setup(
         'pyri.vision.robot_calibration_service': ['*.robdef','*.yml'],
         'pyri.vision.template_matching': ['*.robdef','*.yml']
     },
-    zip_safe=False,
-    install_requires=[
-        'pyri-common',
-        'opencv-contrib-python',
-        'shapely',
-        'RobotRaconteur',
-        'RobotRaconteurCompanion'
-    ],
-    tests_require=['pytest','pytest-asyncio'],
-    extras_require={
-        'test': ['pytest','pytest-asyncio']
-    },
-    entry_points = {
-        'pyri.plugins.sandbox_functions': ['pyri-vision-sandbox-functions=pyri.vision.sandbox_functions:get_sandbox_functions_factory'],
-        'pyri.plugins.device_type_adapter': ['pyri-vision-type-adapter = pyri.vision.device_type_adapter:get_device_type_adapter_factory'],
-        'pyri.plugins.blockly': ['pyri-vision-plugin-blockly=pyri.vision.blockly:get_blockly_factory'],
-        'pyri.plugins.service_node_launch': ['pyri-vision-launch = pyri.vision.service_node_launch:get_service_node_launch_factory'],
-        'console_scripts': ['pyri-vision-aruco-detection-service = pyri.vision.aruco_detection_service.__main__:main',
-            'pyri-vision-camera-calibration-service = pyri.vision.camera_calibration_service.__main__:main',
-            'pyri-vision-camera-viewer-service = pyri.vision.camera_viewer_service.__main__:main',
-            'pyri-vision-robot-calibration-service = pyri.vision.robot_calibration_service.__main__:main',
-            'pyri-vision-template-matching-service = pyri.vision.template_matching.__main__:main'
-        ]
-    }
+    zip_safe=False
 )
